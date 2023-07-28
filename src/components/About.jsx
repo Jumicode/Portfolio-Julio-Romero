4145ae08bbf85829e4d237e1bafd11e6443718a4
Julio Romero <jromeroacive@gmail.com>
2023-07-28 07:03:28 -0430

    About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+ 
+  return (
+    <section id="about" className="py-10 text-white">
+      <div className="text-center mt-8">
+        <h3 className="text-4xl font-semibold">
+          About <span className="text-cyan-600">Me</span>
+        </h3>
+        <p className="text-gray-400 my-3 text-lg">My introduction</p>
+        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+          <div className="p-2">
+            <div className="text-gray-300 my-3">
+              <p className="text-justify leading-7 w-11/12 mx-auto">
+              Computer engineering student, passionate about technology,
+               fan of videogames, anime and music,
+                interest in sports and astronomy,
+                I love programming, I like to learn new technologies and ways to improve
+              </p>
+             
+              <br />
+              <br />
+              <a href="./src/assets/Code_a_program.pdf" download>
+                <button className="btn-primary">Download CV</button>
+              </a>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
